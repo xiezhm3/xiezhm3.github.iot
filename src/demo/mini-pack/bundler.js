c07721ea4baf999586b4aa7e9dd26cccd212f603
xiezhm3 <xiezhm3@mail2.sysu.edu.cn>
2019-01-08 23:45:10 +0800

    update bundler

--- src/demo/mini-pack/bundler.js
+++ src/demo/mini-pack/bundler.js
@@ -1,79 +1,71 @@
-const fs = require('fs');
-const path = require('path');
-const babylon = require('babylon');
-const traverse = require('babel-traverse').default;
-const babel = require('babel-core');
+const fs = require("fs");
+const path = require("path");
+const babylon = require("babylon");
+const traverse = require("babel-traverse").default;
+const babel = require("babel-core");
 
 let ID = 0;
 
 function createAsset(fileName) {
-    const content = fs.readFileSync(fileName, 'utf-8');
+  const content = fs.readFileSync(fileName, "utf-8");
 
-    const ast = babylon.parse(content, {
-        sourceType: 'module'
-    });
+  const ast = babylon.parse(content, {
+    sourceType: "module"
+  });
 
-    const dependencies = [];
+  const dependencies = [];
 
-    traverse(ast, {
-        ImportDeclaration: ({
-            node
-        }) => {
-            dependencies.push(node.source.value);
-        }
-    });
+  traverse(ast, {
+    ImportDeclaration: ({ node }) => {
+      dependencies.push(node.source.value);
+    }
+  });
 
-    const {
-        code
-    } = babel.transformFromAst(ast, null, {
-        presets: ['env']
-    });
+  const { code } = babel.transformFromAst(ast, null, {
+    presets: ["env"]
+  });
 
-    const id = ID++;
+  const id = ID++;
 
-    return {
-        id,
-        fileName,
-        dependencies,
-        code
-    };
+  return {
+    id,
+    fileName,
+    dependencies,
+    code
+  };
 }
 
 function createGraph(entry) {
-    const mainAsset = createAsset(entry);
+  const mainAsset = createAsset(entry);
 
-    const queue = [mainAsset];
+  const queue = [mainAsset];
 
-    for (const asset of queue) {
-        const dirname = path.dirname(asset.fileName);
-        asset.mapping = {};
-        asset.dependencies.forEach((relativePath) => {
-            const absolutePath = path.join(dirname, relativePath);
-            console.log(absolutePath);
-
-            const child = createAsset(absolutePath);
-            asset.mapping[relativePath] = child.id;
-            queue.push(child);
-        });
-    }
-
-    return queue;
+  for (const asset of queue) {
+    const dirname = path.dirname(asset.fileName);
+    asset.mapping = {};
+    asset.dependencies.forEach(relativePath => {
+      const absolutePath = path.join(dirname, relativePath);
+      const child = createAsset(absolutePath);
+      asset.mapping[relativePath] = child.id;
+      queue.push(child);
+    });
+  }
+  return queue;
 }
 
 function bundle(graph) {
+  let modules = "";
 
-    let modules = '';
-
-    graph.forEach(mod => {
-        modules += `${mod.id}: [
+  graph.forEach(mod => {
+    modules += `${mod.id}: [
             function(require, module, exports) {
                 ${mod.code}
             },
             ${JSON.stringify(mod.mapping)}
         ],`;
-    });
+  });
 
-    const result = `
+  const result = `
         (function(modules) {
             function require(id) {
                 const [fn, mapping] = modules[id];
@@ -91,9 +83,9 @@ function bundle(graph) {
         })({${modules}});
     `;
 
-    return result;
+  return result;
 }
 
-const graph = createGraph('./example/entry.js');
+const graph = createGraph("./example/entry.js");
 const result = bundle(graph);
-console.log(result);
+console.log(graph);
