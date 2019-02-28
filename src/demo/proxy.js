125becfa2bfbc0fa8f530f4ee6a98e1beea793de
xiezhm3 <xiezhm3@mail2.sysu.edu.cn>
2019-02-28 08:22:04 +0800

    proxy demo

---
+++ src/demo/proxy.js
@@ -0,0 +1,32 @@
+let target = {
+  name: "target",
+  value: 41
+};
+
+let proxy = new Proxy(target, {
+  set(trapTarget, key, value, receiver) {
+    if (!trapTarget.hasOwnProperty(key) && isNaN(value)) {
+      throw new Error("must be a number!");
+    }
+    return Reflect.set(trapTarget, key, value, receiver);
+  },
+  get(trapTarget, key, receiver) {
+    if (!key in trapTarget) {
+      throw new Error(key + " does not exit!");
+    }
+    return Reflect.get(trapTarget, key, receiver);
+  },
+  has(trapTarget, key) {
+    // key in target
+    if (key === "value") {
+      return false;
+    }
+    return Reflect.has(trapTarget, key);
+  },
+  deleteProperty(trapTarget, key) {
+    if (key === "value") {
+      return false; // 'value' cannot be deleted
+    }
+    return Reflect.deleteProperty(trapTarget, key);
+  }
+});
