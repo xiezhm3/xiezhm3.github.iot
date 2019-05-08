531fc759bc44e5f3df5a656d7f90250b5498eafa
xiezhm3 <xiezhm3@mail2.sysu.edu.cn>
2019-05-08 15:18:41 +0800

    897

---
+++ src/blog/leetcode/897_increasing_order_search_tree.md
@@ -0,0 +1,8 @@
+### 897. Increasing Order Search Tree
+
+> Given a tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only 1 right child.
+
+In short，中序遍历二叉树，然后根据得到的数组顺序构建一个只有右子树的二叉树。
+
+see the solution here: [897. Increasing Order Search Tree](../../leetcode/tree/897_increasing_order_search_tree.js)
+
---
+++ src/leetcode/tree/897_increasing_order_search_tree.js
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+// iteration
+var increasingBST = function (root) {
+    var stack = [];
+    var dummy = new TreeNode(0);
+    var p = dummy;
+    while (root !== null || stack.length) {
+        if (root !== null) {
+            stack.push(root);
+            root = root.left;
+        } else {
+            var cur = stack.pop();
+            root = cur.right;
+            cur.left = null; // TLE
+            p.right = cur;
+            p = p.right;
+        }
+    }
+    return dummy.right;
+};
+
+var increasingBSTRecursive01 = function (root) {
+    var dummy = new TreeNode(0);
+    var p = dummy;
+    var vals = [];
+    inorder(root, vals);
+    for (var i = 0; i < vals.length; i++) {
+        var t = new TreeNode(vals[i]);
+        p.right = t;
+        p = p.right;
+    }
+    return dummy.right;
+};
+var inorder = function (root, vals) {
+    if (root === null) {
+        return;
+    }
+    inorder(root.left.vals);
+    vals.push(root.val);
+    inorder(root.right.vals);
+};
+
+// the most efficient way among these 3 ways
+var p;
+var increasingBSTRecursive02 = function (root) {
+    var dummy = new TreeNode(0);
+    p = dummy;
+    inorder02(root);
+    return dummy.right;
+};
+
+function inorder02(node) {
+    if (node === null) {
+        return;
+    }
+    inorder02(node.left);
+    p.right = node;
+    node.left = null;
+    p = p.right;
+    inorder02(node.right);
+}
